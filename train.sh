f3e8275ba4e5afef2bee78f14a39b68a7dcbfebc
hassanmohsin <mhassan@miners.utep.edu>
2020-02-11 15:08:21 -0700

    Update parameters

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 # original kdeep 
-python train.py --dataroot ~/work/pdbbind/2018/refined-set \
-                --csvfile ../data/refined_set.csv --model kdeep \
-                --gpu_ids 0 --batch_size 256 --nThreads 16 \
-                --lr 0.0001 --niter 5 --niter_decay 5 \
-                --channels cno --rvdw 2 \
+python train.py --dataroot ./dataset/refined-set \
+                --csvfile ./data/refined_set.csv --model kdeep \
+                --gpu_ids 0,1,2,3,4,5,6,7 --batch_size 256 --nThreads 56 \
+                --lr 0.0001 --niter 50 --niter_decay 5 \
+                --channels kdeep --rvdw 2 \
                 --save_epoch_freq 5 --init_type kaiming

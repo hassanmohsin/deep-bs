ff59b3d59c1b3c56737f377ac5f05921a0a66ef5
hassanmohsin <mhassan@miners.utep.edu>
2020-02-11 18:10:33 -0700

    Update for kdeep model training

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 # original kdeep 
 python train.py --dataroot ./dataset/refined-set \
                 --csvfile ./data/refined_set.csv --model kdeep \
-                --gpu_ids 0,1,2,3,4,5,6,7 --batch_size 256 --nThreads 56 \
-                --lr 0.0001 --niter 50 --niter_decay 5 \
-                --channels kdeep --rvdw 2 \
-                --save_epoch_freq 5 --init_type kaiming
+                --gpu_ids 0,1,2,3,4,5,6,7 --batch_size 128 --nThreads 56 \
+                --lr 0.0001 --niter 100 --niter_decay 5 \
+                --channels kdeep --grid_method kdeep
+                --save_epoch_freq 5 --init_type kaiming \
+                --input_nc 8 --print_freq 5 --rvdw 2.0
